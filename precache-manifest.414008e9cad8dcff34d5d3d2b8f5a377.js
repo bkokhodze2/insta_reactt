b07d431ad224f3878fe7412005c099ddb513706d
bakuri kokhodze <b_kokhodze2@cu.edu.ge[=]'cleargit config --global user.email b>
2020-10-28 14:04:35 +0400

    Updates

---
+++ precache-manifest.414008e9cad8dcff34d5d3d2b8f5a377.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e922b7cef227b540c28749f569767ef",
+    "url": "/insta_reactt/index.html"
+  },
+  {
+    "revision": "b469ccad426fc4c7f5c5",
+    "url": "/insta_reactt/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "5b624026fd70fa1ec31c",
+    "url": "/insta_reactt/static/css/main.79ca5f0f.chunk.css"
+  },
+  {
+    "revision": "b469ccad426fc4c7f5c5",
+    "url": "/insta_reactt/static/js/2.920274fa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/insta_reactt/static/js/2.920274fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b624026fd70fa1ec31c",
+    "url": "/insta_reactt/static/js/main.90c08139.chunk.js"
+  },
+  {
+    "revision": "ce877133ffed49d793df",
+    "url": "/insta_reactt/static/js/runtime-main.46950626.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/insta_reactt/static/media/error.3b8ad90a.png"
+  }
+]);
